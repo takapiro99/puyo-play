8259796aa84526cc55d64d89326b44b1e19adbb6
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-06-30 16:46:24 +0900

    init

---
+++ src/game.js
@@ -0,0 +1,96 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  initialize();
+  loop();
+});
+
+let mode;
+let frame;
+let combinationCount;
+
+const initialize = () => {
+  PuyoImage.initialize();
+  Stage.initialize();
+  Player.initialize();
+  Score.initialize();
+  mode = "start";
+  frame = 0;
+};
+
+const loop = () => {
+  switch (mode) {
+    case "start":
+      mode = "checkfall";
+      break;
+    case "checkfall":
+      if (Stage.checkFall()) {
+        mode = "fall";
+      } else {
+        mode = "checkErase";
+      }
+      break;
+    case "fall":
+      if (!Stage.fall()) {
+        mode = "checlErase";
+      }
+      break;
+    case "checkErase":
+      const eraseInfo = Stage.checkErase();
+      if (eraseInfo) {
+        mode = "erasing";
+        combinationCount++;
+        Score.calculateScore(
+          combinationCount,
+          eraseInfo.piece,
+          eraseInfo.color
+        );
+        Stage.hideZenkeshi();
+      } else {
+        if (Stage.puyoCount === 0 && combinationCount > 0) {
+          Stage.showZenkeshi();
+        }
+        combinationCount = 0;
+        mode = "newPuyo";
+      }
+      break;
+    case "erasing":
+      if (!Stage.erasing(frame)) {
+        mode = "checkFall";
+      }
+      break;
+    case "newPuyo":
+      if (!Player.createNewPuyo) {
+        mode = "gameOver";
+      } else {
+        mode = "Playing";
+      }
+      break;
+    case "playing":
+      const action = Player.playing(frame);
+      mode = "action"; // playing, moving, rotating, fixのどれか
+    case "moving":
+      if (!Player.moving(frame)) {
+        mode = "playing";
+      }
+      break;
+    case "rotating":
+      if (!Player.rotating(frame)) {
+        mode = "player";
+      }
+      break;
+    case "fix":
+      Player.fix();
+      mode = "checkFall";
+      break;
+    case "gameOver":
+      PuyoImage.prepareBatankyu(frame);
+      mode = "batankyu";
+    case "batankyu":
+      PuyoImage.batankyu(frame);
+      Player.batankyu();
+      break;
+  }
+  frame++;
+  requestAnimationFrame(loop);
+};
---
+++ src/stage.js
@@ -0,0 +1,3 @@
+class Stage {
+  static initialize() {}
+}
